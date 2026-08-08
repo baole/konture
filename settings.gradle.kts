f3a4978c2a538a2ae4a85d5e0df81cb983f3de58
baole <leducbao@gmail.com>
2026-08-08 09:54:17 +0200

    reconfigure konture-test module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,14 @@ include("core")
 include("library")
 include("plugin-gradle")
 
-includeBuild("konture-test")
+val isIdeSync = System.getProperty("idea.active") == "true" ||
+    System.getProperty("idea.sync.active") == "true" ||
+    providers.systemProperty("idea.active").orNull == "true" ||
+    providers.systemProperty("idea.sync.active").orNull == "true" ||
+    providers.systemProperty("konture.includeTest").orNull == "true"
+
+if (isIdeSync) {
+    includeBuild("konture-test")
+}
 
 
